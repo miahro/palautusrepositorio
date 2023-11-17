340845003236e406fc047247240d8b82c56d3035
miahro <mikko.ahro@gmail.com>
2023-11-17 20:21:40 +0200

    W4/Ex4

--- viikko4/verkkokauppa/src/tests/kauppa_test.py
+++ viikko4/verkkokauppa/src/tests/kauppa_test.py
@@ -18,12 +18,16 @@ class TestKauppa(unittest.TestCase):
                 return 10
             if tuote_id == 2:
                 return 0
+            if tuote_id == 3:
+                return 10
 
         def varasto_hae_tuote(tuote_id):
             if tuote_id == 1:
                 return Tuote(1, "maito", 5)
             if tuote_id == 2:
                 return Tuote(2, "voi", 10)
+            if tuote_id == 3:
+                return Tuote(3, "leipä", 15)
 
         varasto_mock.saldo.side_effect = varasto_saldo
         varasto_mock.hae_tuote.side_effect = varasto_hae_tuote
@@ -63,3 +67,27 @@ class TestKauppa(unittest.TestCase):
 
         self.pankki_mock.tilisiirto.assert_called_with(
             "pekka", 42, "12345", "33333-44455", 5)
+
+    def test_aloita_asiointi_nollaa_edelliset_ostokset(self):
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.tilimaksu("pekka", "12345")
+
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(1)
+
+        self.kauppa.tilimaksu("pekka", "12345")
+
+        self.pankki_mock.tilisiirto.assert_called_with(
+            "pekka", 42, "12345", "33333-44455", 5)
+
+    def test_poista_tuote_paivittaa_tiedot_oikein(self):
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.lisaa_koriin(3)
+        self.kauppa.poista_korista(1)
+        self.kauppa.tilimaksu("pekka", "12345")
+
+        self.pankki_mock.tilisiirto.assert_called_with(
+            "pekka", 42, "12345", "33333-44455", 15)
